3c71227069ec94c6c8e5ee5169b103adf5c509ad
Rishi P. <rishiparkhe@outlook.com>
2015-02-25 20:49:25 +0530

    added initial solution...

---
+++ TFS_Streak/StreakViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS_Streak
+{
+    public class StreakViewModel
+    {
+        private const int DaysInOneYear = 365;
+        private const int DayInAWeek = 7;
+
+        private IList<WeekViewModel> WeekViewModels { get; set; }
+        public ICollectionView Weeks { get; private set; }
+        DateTime _oneYearBack;
+        private DateTime _today;
+
+        public StreakViewModel(DateTime today)
+        {
+            _today = today;
+            _oneYearBack = today - new TimeSpan(365, 0, 0, 0, 0);
+
+            WeekViewModels = new List<WeekViewModel>();
+            PopulateWeeks();
+        }
+
+        private void PopulateWeeks()
+        {
+            int numOfWeeks = DaysInOneYear / DayInAWeek;
+
+
+        }
+    }
+}
---
+++ TFS_Streak/WeekViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TFS_Streak
+{
+    public class WeekViewModel
+    {
+    }
+}
